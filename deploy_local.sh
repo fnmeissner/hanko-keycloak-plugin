f2c747fac6b8b20506223fa8866efa6e56df32ab
Felix Meißner <felix.meissner@hanko.io>
2018-11-14 14:28:50 +0100

    fix deploy script

--- deploy_local.sh
+++ deploy_local.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
 mvn package
-vagrant scp hanko-plugin-keycloak-ear/target/hanko-plugin-keycloak.ear :/opt/keycloak-4.1.0.Final/standalone/deployments/hanko-plugin-keycloak.ear
-vagrant scp themes/base/login/login-hanko.ftl :/opt/keycloak-4.1.0.Final/themes/base/login/login-hanko.ftl
-vagrant scp themes/keycloak/login/resources/img/login-hanko.png :/opt/keycloak-4.1.0.Final/themes/keycloak/login/resources/img/login-hanko.png
-vagrant scp themes/base/account/account-hanko.ftl :/opt/keycloak-4.1.0.Final/themes/base/account/account-hanko.ftl
-vagrant scp themes/keycloak/login/resources/js :/opt/keycloak-4.1.0.Final/themes/keycloak/login/resources/js
+vagrant scp hanko-plugin-keycloak-ear/target/hanko-plugin-keycloak.ear :/opt/keycloak-4.5.0.Final/standalone/deployments/hanko-plugin-keycloak.ear
+vagrant scp themes/base/login/login-hanko.ftl :/opt/keycloak-4.5.0.Final/themes/base/login/login-hanko.ftl
+vagrant scp themes/keycloak/login/resources/img/login-hanko.png :/opt/keycloak-4.5.0.Final/themes/keycloak/login/resources/img/login-hanko.png
+vagrant scp themes/keycloak/login/resources/js/hanko.js :/opt/keycloak-4.5.0.Final/themes/keycloak/login/resources/js/hanko.js
+vagrant scp themes/base/account/account-hanko.ftl :/opt/keycloak-4.5.0.Final/themes/base/account/account-hanko.ftl
+vagrant scp themes/keycloak/login/resources/js/fetch.umd.min.js :/opt/keycloak-4.5.0.Final/themes/keycloak/login/resources/js/fetch.umd.min.js
+vagrant scp themes/keycloak/login/resources/js/polyfill.min.js :/opt/keycloak-4.5.0.Final/themes/keycloak/login/resources/js/polyfill.min.js
+vagrant scp themes/keycloak/login/resources/js/hanko.js :/opt/keycloak-4.5.0.Final/themes/keycloak/login/resources/js/hanko.js
+vagrant scp themes/keycloak/account/resources/js/fetch.umd.min.js :/opt/keycloak-4.5.0.Final/themes/keycloak/account/resources/js/fetch.umd.min.js
+vagrant scp themes/keycloak/account/resources/js/polyfill.min.js :/opt/keycloak-4.5.0.Final/themes/keycloak/account/resources/js/polyfill.min.js
