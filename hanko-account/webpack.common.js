44ac720409bb99f287d7bde612acd6db792ac0a1
Felix Meißner <felix.meissner@hanko.io>
2018-11-22 12:37:12 +0100

    babel

--- hanko-account/webpack.common.js
+++ hanko-account/webpack.common.js
@@ -25,6 +25,16 @@ module.exports = {
         type: 'javascript/auto'
       },
       {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
         test: /\.scss$/,
         use: [
           'style-loader', // creates style nodes from JS strings
