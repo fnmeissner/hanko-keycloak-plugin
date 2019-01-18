e4981a9bfad99d152c36e425a027fac50b3419c6
Felix Meißner <felix.meissner@hanko.io>
2019-01-18 10:40:06 +0100

    update deploy vagrant script

--- deploy_vagrant.sh
+++ deploy_vagrant.sh
@@ -2,9 +2,12 @@
 
 mvn package
 vagrant scp hanko-plugin-keycloak-ear/target/hanko-plugin-keycloak.ear :/opt/keycloak-4.5.0.Final/standalone/deployments/hanko-plugin-keycloak.ear
+vagrant scp themes/base/login/hanko-multi-login.ftl :/opt/keycloak-4.5.0.Final/themes/base/login/hanko-multi-login.ftl
+vagrant scp themes/base/login/hanko-multi-login-links.ftl :/opt/keycloak-4.5.0.Final/themes/base/login/hanko-multi-login-links.ftl
+vagrant scp themes/base/login/hanko-username.ftl :/opt/keycloak-4.5.0.Final/themes/base/login/hanko-username.ftl
 vagrant scp themes/base/login/login-hanko.ftl :/opt/keycloak-4.5.0.Final/themes/base/login/login-hanko.ftl
-vagrant scp themes/keycloak/login/resources/img/login-hanko.png :/opt/keycloak-4.5.0.Final/themes/keycloak/login/resources/img/login-hanko.png
 vagrant scp themes/base/account/account-hanko.ftl :/opt/keycloak-4.5.0.Final/themes/base/account/account-hanko.ftl
+vagrant scp themes/keycloak/login/resources/img/login-hanko.png :/opt/keycloak-4.5.0.Final/themes/keycloak/login/resources/img/login-hanko.png
 vagrant scp themes/keycloak/login/resources/js :/opt/keycloak-4.5.0.Final/themes/keycloak/login/resources
 vagrant scp themes/keycloak/account/resources/js :/opt/keycloak-4.5.0.Final/themes/keycloak/account/resources
 vagrant scp themes/hanko :/opt/keycloak-4.5.0.Final/themes
