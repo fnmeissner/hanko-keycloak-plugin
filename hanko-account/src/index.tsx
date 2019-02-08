c59fecb35f6a8ef3eb445eb33e18ffb99153fc39
Felix Meißner <felix.meissner@hanko.io>
2019-02-08 11:43:09 +0100

    hanko-account: add even more debug output regarding __webpack_public_path__

--- hanko-account/src/index.tsx
+++ hanko-account/src/index.tsx
@@ -31,6 +31,7 @@ const keycloak = Keycloak({
 console.log('baseUrl: ', window.resourceBaseUrl)
 
 if (window.resourceBaseUrl) {
+  console.log('setting baseUrl to:', window.resourceBaseUrl)
   window.__webpack_public_path__ = window.resourceBaseUrl
 }
 
