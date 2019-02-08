b3280631c0148fdd07e7bf1b5ca700cdb5a26d0b
Felix Meißner <felix.meissner@hanko.io>
2019-02-08 12:24:42 +0100

    hanko-account: set __webpack_public_path__ as global variable

--- hanko-account/src/utils/publicPath.ts
+++ hanko-account/src/utils/publicPath.ts
@@ -1,6 +1,9 @@
 console.log('baseUrl: ', window.resourceBaseUrl)
 
+declare var __webpack_public_path__: string | undefined
+
 if (window.resourceBaseUrl) {
   console.log('setting baseUrl to:', window.resourceBaseUrl)
   window.__webpack_public_path__ = window.resourceBaseUrl
+  __webpack_public_path__ = window.resourceBaseUrl
 }
